826935a37aeeaa5e96bf244f24e6f6d425cc606d
jdankowski <jakudan881@student.polsl.pl>
2020-04-06 05:26:57 +0200

    Demo

---
+++ MobRISv2/MobRISv2/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MobRISv2.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobRISv2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new MyNavigationPage());
+            MainPage = new NavigationPage(new MyNavigationPage2()) { BarBackgroundColor = Color.GreenYellow, BarTextColor = Color.Red };
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobRISv2/MobRISv2/Views/MyNavigationPage2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MobRISv2.Views
+{
+    public class MyNavigationPage2 : ContentPage
+    {
+        public MyNavigationPage2()
+        {
+            this.Title = "Navigation Page 2";
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "My navigation Page 2" }
+                }
+            };
+        
+            ToolbarItems.Add(new ToolbarItem("Add", "addicon.png", new Action(() => { })));
+
+            var deleteItem = new ToolbarItem();
+            deleteItem.Text = "Delete";
+            deleteItem.Order = ToolbarItemOrder.Secondary;
+            this.ToolbarItems.Add(deleteItem);
+        
+        
+        }
+
+    }
+}
