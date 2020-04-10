7453dc9042743ad4066fed0232646333099e4863
jdankowski <jakudan881@student.polsl.pl>
2020-04-10 21:10:35 +0200

    Swipe

---
+++ MobRISv2-master/MobRISv2-master/MobRISv2/MobRISv2/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MobRISv2.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobRISv2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new MyNavigationPage());
+            //MainPage = new NavigationPage(new MyNavigationPage2()) { BarBackgroundColor = Color.GreenYellow, BarTextColor = Color.Red };
+            //MainPage = new TabbedPage();
+
+            //TabbedPage tabbedPage = new TabbedPage();
+            //tabbedPage.Children.Add(new MyContentPage() { Title="My Contenet Page 1 "});
+            //tabbedPage.Children.Add(new MyContentPage2());
+
+            //MainPage = tabbedPage;
+
+            //MainPage = new MyCarouselPage();
+
+            CarouselPage carouselPage = new CarouselPage();
+            carouselPage.Children.Add(new MyContentPage());
+            carouselPage.Children.Add(new MyContentPage2());
+
+            MainPage = carouselPage;
+
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobRISv2-master/MobRISv2-master/MobRISv2/MobRISv2/Views/MyCarouselPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MobRISv2.Views
+{
+    public class MyCarouselPage : CarouselPage
+    {
+        public MyCarouselPage()
+        {
+            Children.Add(new MyContentPage());
+            Children.Add(new MyContentPage2());
+        }
+    }
+}
